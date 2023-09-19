736bbed17882bb60c89e670c38b863b50af46ce5
Jonathan <jonathan.ramos@ikasle.aeg.eus>
2023-09-19 08:45:42 +0200

    [NEW] Completada la fase 2

--- index.js
+++ index.js
@@ -15,11 +15,15 @@ const fetchAsyncData = async()=>{
         donutSugar(items);
         donutHierro(items);
         donutProteine(items);
-        donutFibre(items)
+        donutFibre(items);
+        listDonutCalories(items);
+        listDonutCarbohydrates(items);
+        donutCaloriesAverage(items);
+        donutSaturatedFatSum(items);
+        vitamineAverage(items);
 
-        console.log(result.items.item[4].nutrition_facts.nutrition.proteine)
+        console.log(result.items.item[4].nutrition_facts.nutrition.vitamines[0])
 
-        return console.log(result);
     }catch(error){
 
         console.log(error.message)
@@ -71,10 +75,56 @@ function donutFibre(items){
     items.filter(item=> parseInt(item.nutrition_facts.nutrition.carbohydrate.carbs_detail.type.fibre) === minFibreValue).map(item=> console.log(`El Donut con Menos Fibra es: ${item.name}`));
 }
 
+///////////////////
+/////Fase 2
+///////////////////
+
+function listDonutCalories(items){
 
 
+    items.map(item=>console.log(`El donut llamado ${item.name} aporta un total de ${item.nutrition_facts.nutrition.calories} calorías`));
+}
 
 
+function listDonutCarbohydrates(items){
 
 
+    items.map(item=>console.log(`El donut llamado ${item.name} contiene ${item.nutrition_facts.nutrition.carbohydrate.carbs_detail.amount} de carbohidratos`));
+}
 
+
+function donutCaloriesAverage(items){
+
+
+    const caloriesAverage = items.reduce((count, item)=> count + item.nutrition_facts.nutrition.calories, 0)/items.length;
+    console.log(`La media de calorías de los donuts es de: ${caloriesAverage}`)
+}
+
+
+function donutSaturatedFatSum(items){
+
+
+    const saturatedFatSum = items.reduce((count, item)=> count + parseInt(item.nutrition_facts.nutrition.fat.fat_type.saturated), 0);
+    console.log(`La suma de las grasas saturadas de los donuts es de: ${saturatedFatSum}`)
+}
+
+function vitamineAverage(items){
+
+
+    const vitamineValues = items[0].nutrition_facts.nutrition.vitamines.filter(vitamine => vitamine);
+    vitamineValues.map(vitamine=>  vitamine.percent = 0);
+
+
+    items.map(item=> item.nutrition_facts.nutrition.vitamines.map(vitamine=> {
+
+        vitamineValues.map(vitamineValuesVitamine=>{
+
+            if(vitamineValuesVitamine.type === vitamine.type){
+
+                vitamineValuesVitamine.percent =+ parseInt(vitamine.percent);
+            }
+        })
+    }))
+
+    vitamineValues.map(vitamine=> console.log(`El porcentaje medio de la vitamina ${vitamine.type} en los donuts es del ${vitamine.percent/items.length}%.`))
+}
